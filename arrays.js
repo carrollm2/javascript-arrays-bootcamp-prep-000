1d1bfa8b6925402e5d84ee98b4a6ffbbf9d8914e
Michael Carroll <carrollm2@gmail.com>
2017-11-13 20:14:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,15 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var newArray = array.unshift(element)
+  
+  var newArray = new Array();
+  newArray = [element, ...array];
   return newArray
 
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
 
-  array[0] = element;
-  return array
-
+  return array.unshift(element);
 
 }
